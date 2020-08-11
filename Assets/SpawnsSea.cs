e7dd840d24985d3966864f06b9b91e388bd36f09
Conterry <59209260+Conterry@users.noreply.github.com>
2020-08-11 21:41:03 +0300

    WIP

---
+++ Assets/SpawnsSea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnsSea : MonoBehaviour
+{
+    public GameObject SeaSpawn;
+    public GameObject SeaPrefab;
+    public int GameSpeed;
+
+    public void OnTriggerEnter(Collider SeaPrefab)
+    {
+        SpawnSea();
+    }
+
+    public void SpawnSea()
+    {
+        GameObject sea = Instantiate(SeaPrefab, SeaSpawn.transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/StartSeamouve.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSeamouve : MonoBehaviour
+{
+    
+    public GameObject Sea;
+    public int GameSpeed;
+
+    void Update()
+    {
+        this.transform.Translate(Vector3.back * GameSpeed);
+    }
+
+}
